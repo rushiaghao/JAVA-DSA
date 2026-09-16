405d0aefcec9db8d8071d05105a3f3cbbdf9b872
rushiaghao <159922576+rushiaghao@users.noreply.github.com>
2026-09-16 22:29:44 +0530

    Time: 17 ms (7.52%), Space: 47.4 MB (5.66%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 2716-minimize-string-length/2716-minimize-string-length.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int minimizedStringLength(String s) {
+        return (int) s.chars().distinct().count();
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
