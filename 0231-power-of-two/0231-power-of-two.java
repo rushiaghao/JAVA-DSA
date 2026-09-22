613d85366d882efbb4168314d3737c24f79cebc9
rushiaghao <159922576+rushiaghao@users.noreply.github.com>
2026-09-22 22:41:54 +0530

    Time: 1 ms (96.62%), Space: 42.7 MB (36.66%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) return false;
+        while (n % 2 == 0) {
+            n /= 2;
+        }
+        return n == 1;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
