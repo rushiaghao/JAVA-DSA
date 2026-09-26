d79bf4af5512a9bfa6a01838860c8645e9f2ca78
rushiaghao <159922576+rushiaghao@users.noreply.github.com>
2026-09-26 19:59:19 +0530

    Time: 1 ms (98.15%), Space: 42.6 MB (62.9%) - LeetHub - Time: O(log(num)), Space: O(1)

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int addDigits(int num) {
+        while (num >= 10) {
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
